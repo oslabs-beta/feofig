ba82cc5ae597b6047ddd2bf1c833ba12bfc58097
keidy9 <keidyw999@gmail.com>
2023-11-30 10:57:26 -0800

    initial mvp-test ready >> >> >> Co-authored-by: Zack <zackaryrauzi@gmail.com> >> Co-authored-by: Cristina <cristinalee628@gmail.com> >> Co-authored-by: Andy <axm.lam@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?/,
-        exclude: /node_modules/,
+        // exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
