a8aa8063ae1323e21d156af1279025887f3f205f
keidy9 <keidyw999@gmail.com>
2023-12-01 01:13:24 -0800

    rewrote feofig.js using a different method >> >> >> Co-authored-by: Zack <zackaryrauzi@gmail.com> >> Co-authored-by: Cristina <cristinalee628@gmail.com> >> Co-authored-by: Andy <axm.lam@gmail.com>

--- package/feofig.js
+++ package/feofig.js
@@ -12,18 +12,31 @@ const Fig = ({ children, config, placeholder }) => {
     if (Array.isArray(child.props.children)) {
       // console.log(child.props.children)
       const newArray = child.props.children.map((child) => {
+        // console.log(child)
         return recursiveWrap(child);
       });
-      
-      return newArray;
 
-    } 
-    else if (typeof child.props.children !== 'object') { // this case is testing for just strings, so it will lazy load the div that only has a string for children. In LazyLoad.js, need to set the innerHTML equal to child, since child is a string.
-      return <LazyLoad key={crypto.randomUUID()} renderInnerHTML={true}>{child}</LazyLoad>
-    }
-    
-    else {
-      return <LazyLoad key={crypto.randomUUID()}>{child}</LazyLoad>;
+      return newArray;
+    } else if (typeof child.props.children === 'string') {
+      // this case is testing for just strings, so it will lazy load the div that only has a string for children. In LazyLoad.js, need to set the innerHTML equal to child, since child is a string.
+      return (
+        <LazyLoad
+          key={crypto.randomUUID()}
+          renderInnerHTML={true}
+          placeholder={placeholder}
+        >
+          {child}
+        </LazyLoad>
+      );
+    } else if (typeof child.props.children === 'object') {
+      console.log(child.props.children)
+      return recursiveWrap(child.props.children); // the issue here is that this needs to return <LazyLoad>{recursiveWrap(child.props.children)}</LazyLoad> but it cant because it'll throw an error: 'cant read properties of undefined, reading "type"'.
+    } else {
+      return (
+        <LazyLoad key={crypto.randomUUID()} placeholder={placeholder} a='a'>
+          {child}
+        </LazyLoad>
+      );
     }
   };
 
--- package/utils/lazyload.js
+++ package/utils/lazyload.js
@@ -1,10 +1,128 @@
 import React, { useEffect, useRef, useState } from 'react';
 
-const LazyLoad = ({children, renderInnerHTML}) => {
+const LazyLoad = ({ children, renderInnerHTML, threshold, placeholder }) => {
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: threshold || 0.5,
+    };
+
+    const handleIntersection = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const element = entry.target;
+          if (element.nodeName === 'IMG') {
+            element.className = children.props.className; // replace placeholder className to img className
+            element.src = children.props.src; // replace placeholder src to img src
+            observer.unobserve(element);
+          } else {
+            element.innerHTML = children.props.children;
+            observer.unobserve(element);
+          }
+        }
+      });
+    };
+    const observer = new IntersectionObserver(handleIntersection, options);
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
+      }
+    };
+  }, []);
+
   // console.log(children)
-  const newReactElement = React.cloneElement(children, { hello: '69', renderInnerHTML: renderInnerHTML })
-  console.log(newReactElement)
+
+  const returnRenderedElement = (children) => {
+    if (children.type === 'img') {
+      if (placeholder)
+        return <>{React.cloneElement(placeholder, { ref: elementRef })}</>;
+      else
+        return (
+          <>{React.cloneElement(children, { ref: elementRef, src: null })}</>
+        );
+    } else
+      return (
+        <>{React.cloneElement(children, { ref: elementRef, children: null })}</>
+      );
+  };
+
+  const newReactElement = returnRenderedElement(children);
+  // console.log(newReactElement);
+  // console.log(children)
+
   return newReactElement;
 };
 
 export default LazyLoad;
+
+// import React, { useEffect, useRef } from 'react';
+
+// const LazyLoad = ({ children, src, alt, className, threshold, placeholder }) => {
+//   const imgRef = useRef(null);
+
+//   useEffect(() => {
+//     const options = {
+//       root: null,
+//       rootMargin: '0px',
+//       threshold: threshold,
+//     };
+
+//     const handleIntersection = (entries, observer) => {
+//       entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//           const element = entry.target;
+//           element.className = children.props.className;
+
+//           if (element.nodeName === 'IMG') {
+//             element.src = children.props.src;
+//             observer.unobserve(element);
+//           } else {
+//             const childProps = [];
+//             if (Array.isArray(children.props.children)) {
+//               // console.log("@@@@@@@@@@@@@: ", children.props.children.length, "!!!!!!!!!!: ", children.props.children)
+//               children.props.children.forEach((prop) => {
+//                 if (typeof prop !== "object") childProps.push(prop)
+//               })
+//           } else childProps.push(children.props.children)
+//             element.innerHTML = childProps;
+//             observer.unobserve(element);
+//           }
+//         }
+//       });
+//     };
+
+//     const observer = new IntersectionObserver(handleIntersection, options);
+
+//     if (imgRef.current) {
+//       observer.observe(imgRef.current);
+//     }
+
+//     return () => {
+//       if (imgRef.current) {
+//         observer.unobserve(imgRef.current);
+//       }
+//     };
+//   }, []); // Empty dependency array to run the effect only once on mount
+
+//   const returnRenderedElement = (child) => {
+//     if (child.type === 'img'){
+//       if (placeholder) return <>{React.cloneElement(placeholder, { ref: imgRef})}</>;
+//       else
+//       return <>{React.cloneElement(children, { ref: imgRef, src: null})}</>;
+//     }
+//     else
+//       return <>{React.cloneElement(children, { ref: imgRef, children: null})}</>;
+//   };
+
+//   const renderedElement = returnRenderedElement(children);
+
+//   return renderedElement
+// };
+
+// export default LazyLoad;
