a6ca2a047e9e91f58584207ea057658a802df924
Andy Lam <axm.lam@gmail.com>
2023-12-01 17:49:09 -0800

    changed input dir to dev and output dir to package. reverted feofig to previous un-nested version. compiled lazyload into package folder  >> >> >> Co-authored-by: Zack <zackaryrauzi@gmail.com> >> Co-authored-by: Cristina <cristinalee628@gmail.com> >> Co-authored-by: Keidy <keidyw999@gmail.com>

---
+++ dev/utils/lazyload.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const LazyLoad = ({ children, threshold, placeholder }) => {
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: threshold || 0.5,
+    };
+
+    const handleIntersection = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const element = entry.target;
+          if (element.nodeName === 'IMG') {
+            element.className = children.props.className; // replace placeholder className to img className
+            element.src = children.props.src; // replace placeholder src to img src
+            observer.unobserve(element);
+          } else {
+            element.innerHTML = children.props.children;
+            observer.unobserve(element);
+          }
+        }
+      });
+    };
+    const observer = new IntersectionObserver(handleIntersection, options);
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
+      }
+    };
+  }, []);
+
+  const returnRenderedElement = (children) => {
+    if (children.type === 'img') {
+      if (placeholder)
+        return React.cloneElement(placeholder, { ref: elementRef });
+      else return React.cloneElement(children, { ref: elementRef, src: null });
+    } else
+      return React.cloneElement(children, { ref: elementRef, children: null });
+  };
+
+  const newReactElement = returnRenderedElement(children);
+
+  return newReactElement;
+};
+
+export default LazyLoad;
