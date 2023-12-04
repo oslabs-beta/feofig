b3eda648c69825093d83ec92412f1ccd67c3a0d1
Cristina Lee <cristinalee628@gmail.com>
2023-12-04 11:53:02 -0800

    added working function for debounce

--- dev/utils/debounce.ts
+++ dev/utils/debounce.ts
@@ -1,12 +1,26 @@
 const Debounce = () => {
   
   // from https://levelup.gitconnected.com/debounce-from-scratch-8616c8209b54
-  const debounce = (func, wait: number) => {
-    let timerId;
-    return () => {
+  const debounce = <T extends (...args: any[]) => void> (func: T, wait: number) => {
+    let timerId: NodeJS.Timeout;
+    return (...args: Parameters<T>) => {
       clearTimeout(timerId);
-      timerId = setTimeout(() => func(), wait);
+      timerId = setTimeout(() => func(...args), wait);
     }
   }
 
+  // // from https://kettanaito.com/blog/debounce-vs-throttle
+  // function debounce(func, duration) {
+  //   let timeout
+  
+  //   return function (...args) {
+  //     const effect = () => {
+  //       timeout = null
+  //       return func.apply(this, args)
+  //     }
+  
+  //     clearTimeout(timeout)
+  //     timeout = setTimeout(effect, duration)
+  //   }
+  // }
 }
