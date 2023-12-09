cd91245c3f94caf5e5f5779d05fdeec956899402
Cristina Lee <cristinalee628@gmail.com>
2023-12-09 15:52:42 -0800

    Debounce and Throttle wrappers in practice

--- client/src/ExampleComponent.jsx
+++ client/src/ExampleComponent.jsx
@@ -1,62 +1,67 @@
 import React, { useState } from 'react';
-import Debounce from '../../dev/utils/debounce';
+import Debounce from '../../dev/utils/debounce.tsx';
 import Home from './Home';
-import Throttle from '../../dev/utils/throttle';
+import Throttle from '../../dev/utils/throttle.tsx';
 
 const ExampleComponent = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchTermThrottle, setSearchTermThrottle] = useState('');
   const [selectedOption, setSelectedOption] = useState('');
 
-  const [buttonClickCount, setButtonClickCount] = useState(0);
-
-  const handleButtonClick = () => {
-    setButtonClickCount((prevCount) => prevCount + 1);
-  };
-
   return (
-    <div>
-      <h1>Debounced Search Example</h1>
-      {/* <Debounce
-        element="input"
-        type="text"
-        onChange={(event) => {setSearchTerm(event.target.value)}}
-        value={searchTerm}
-        minLength={3} // Specify the minimum length before triggering the search
-        debounceTimeout={1000} // Specify the debounce timeout (in milliseconds)
-      /> */}
-      <Debounce
-        onChange={(event) => { setSearchTerm(event.target.value);}}
-        minLength={3}
-        debounceTimeout={1000}
-      >
-        <div>
-          {/* <textarea /> */}
-          <input type='text' />
-        </div>
-      </Debounce>
+    <>
       <div>
-        <strong>Current Search Term:</strong> {searchTerm}
+        <h1>Debounced Search Example</h1>
+        {/* <Debounce
+          element="input"
+          type="text"
+          onChange={(event) => {setSearchTerm(event.target.value)}}
+          value={searchTerm}
+          minLength={3} // Specify the minimum length before triggering the search
+          debounceTimeout={1000} // Specify the debounce timeout (in milliseconds)
+        /> */}
+        <Debounce
+          onChange={(event) => { setSearchTerm(event.target.value);}}
+          minLength={3}
+          debounceTimeout={1000}
+        >
+          <div>
+            {/* <textarea /> */}
+            <input type='text' /> {/* <- input element wrapped with Debounce */}
+          </div>
+        </Debounce>
+        <div>
+          <strong>Current Search Term:</strong> {searchTerm}
+        </div>
       </div>
 
-    </div>
+      <div>
+        <h1>Throttled Search Example</h1>
+        {/* <Throttle
+          element="input"
+          type="text"
+          onChange={(event) => {setSearchTermThrottle(event.target.value)}}
+          value={searchTermThrottle}
+          minLength={0} // Specify the minimum length before triggering the search
+          throttleTimeout={500} // Specify the throttle timeout (in milliseconds)
+        /> */}
+        <Throttle
+          onChange={(event) => { setSearchTermThrottle(event.target.value);}}
+          minLength={0}
+          throttleTimeout={500}
+        >
+          <div>
+            <input type='text' /> {/* <- input element wrapped with Throttle */}
+            {/* <textarea /> */}
+          </div>
+        </Throttle>
+        <div>
+          <strong>Current Search Term:</strong> {searchTermThrottle}
+        </div>
+      </div>
+    </>
 
-    // <div>
-    //   <h1>Throttled Search Example</h1>
-    //   <Throttle
-    //     element="input"
-    //     type="text"
-    //     onChange={(event) => {setSearchTermThrottle(event.target.value)}}
-    //     value={searchTermThrottle}
-    //     minLength={0} // Specify the minimum length before triggering the search
-    //     throttleTimeout={500} // Specify the throttle timeout (in milliseconds)
-    //   />
-    //   <div>
-    //     <strong>Current Search Term:</strong> {searchTermThrottle}
-    //   </div>
-    // </div>
-
-    // select element not properly working; now showing selectedOption next to "Current Selected Option" somehow
+    // select element not properly working; not showing selectedOption next to "Current Selected Option" somehow
     // <div>
     //   <h1>Debounced Select Example</h1>
     //   <Debounce
