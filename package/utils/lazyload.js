a702309e6c9c7c415da7d6ec96cdee5922999b31
keidy9 <keidyw999@gmail.com>
2023-12-11 11:36:39 -0800

    update package for npm install >> >> >> Co-authored-by: Zack <zackaryrauzi@gmail.com> >> Co-authored-by: Cristina <cristinalee628@gmail.com> >> Co-authored-by: Keidy <keidyw999@gmail.com> >> Co-authored-by: Andy <axm.lam@gmail.com>

--- package/utils/lazyload.js
+++ package/utils/lazyload.js
@@ -1,50 +1,31 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importStar(require("react"));
-const LazyLoad = ({ children, threshold, placeholder, }) => {
-    const elementRef = (0, react_1.useRef)(null);
-    (0, react_1.useEffect)(() => {
+import React, { useEffect, useRef } from 'react';
+const LazyLoad = ({ children, threshold = 0, placeholder, once, offset = '0px', }) => {
+    const elementRef = useRef(null);
+    useEffect(() => {
         const options = {
             root: null,
-            rootMargin: '0px',
-            threshold: threshold || 0.5,
+            // PROB NEEDS CHANGE: rootmargin default is '0px 0px 0px 0px'
+            rootMargin: offset, // user config for offset
+            threshold: threshold, // user config for threshold
         };
         const handleIntersection = (entries, observer) => {
             entries.forEach((entry) => {
+                const image = entry.target;
                 if (entry.isIntersecting) {
-                    const element = entry.target;
-                    if (element.nodeName === 'IMG') {
-                        const imgElement = element;
-                        imgElement.className = children.props.className; // replace placeholder className to img className
-                        imgElement.src = children.props.src; // replace placeholder src to img src
-                        observer.unobserve(element);
+                    image.className = children.props.className; // replace placeholder className to img className
+                    image.src = children.props.src; // replace placeholder src to img src
+                    if (once)
+                        observer.unobserve(image);
+                }
+                else if (!once && !entry.isIntersecting) {
+                    // prioritizes local image placeholder if it exists
+                    if (children.props.placeholder) {
+                        image.className = children.props.placeholder.props.className;
+                        image.src = children.props.placeholder.props.src;
                     }
                     else {
-                        element.innerHTML = children.props.children;
-                        observer.unobserve(element);
+                        image.className = placeholder === null || placeholder === void 0 ? void 0 : placeholder.props.className;
+                        image.src = placeholder === null || placeholder === void 0 ? void 0 : placeholder.props.src;
                     }
                 }
             });
@@ -60,16 +41,15 @@ const LazyLoad = ({ children, threshold, placeholder, }) => {
         };
     }, []);
     const returnRenderedElement = (children) => {
-        if (children.type === 'img') {
-            if (placeholder)
-                return react_1.default.cloneElement(placeholder, { ref: elementRef });
-            else
-                return react_1.default.cloneElement(children, { ref: elementRef, src: null });
-        }
+        // prioritizes local image placeholder if it exists
+        if (children.props.placeholder)
+            return React.cloneElement(children.props.placeholder, { ref: elementRef });
+        if (placeholder)
+            return React.cloneElement(placeholder, { ref: elementRef });
         else
-            return react_1.default.cloneElement(children, { ref: elementRef, children: null });
+            return React.cloneElement(children, { ref: elementRef, src: null });
     };
     const newReactElement = returnRenderedElement(children);
     return newReactElement;
 };
-exports.default = LazyLoad;
+export default LazyLoad;
